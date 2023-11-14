d651622e4ff5893fca8d4d566992f59312f1c73e
Diego <73858390+diegodahl@users.noreply.github.com>
2023-11-14 10:31:01 -0500

    Add files via upload

--- methods/jacobi.py
+++ methods/jacobi.py
@@ -1,64 +1,27 @@
 import numpy as np
+from numpy.linalg import norm
 
-# CALCULAR RADIO ESPECTRAL
-def spectral_radius(matrix):
-    eigenvalues, _ = np.linalg.eig(matrix)
+def jacobi_method(A, b, x0, tol, max_iter):
+    D = np.diag(np.diag(A))
+    LU = A - D
+    x = x0
+    convergence = 0
+    for i in range(max_iter):
+        x_new = np.dot(np.linalg.inv(D), b - np.dot(LU, x))
+        if norm(x - x_new) < tol:
+            convergence = 1
+            break
+        x = x_new
+    return x, convergence
+
+def spectral_radius(A):
+    eigenvalues, _ = np.linalg.eig(A)
     return max(abs(eigenvalues))
 
-# CALCULAR MATRIZ T
-def jacobi_T_matrix(A):
-    n = A.shape[0]
-    T = np.zeros((n, n))
-    
-    for i in range(n):
-        for j in range(n):
-            if i != j:
-                T[i][j] = -A[i][j] / A[i][i]
-    
-    return T
+def solve_jacobi(A, b, x0, tol, max_iter):
+    spectral_r = spectral_radius(A)
+    sol, convergence = jacobi_method(A, b, x0, tol, max_iter)
+    return {"radio": spectral_r, "converge": convergence, "sol": sol}
 
-def jacobi(A, b, x0, tol, max_iter):
-    n = len(A)
-    x = x0.copy()
-    tmp = 0
-    for iteration in range(max_iter):
-        x_new = np.zeros(n)
-        for i in range(n):
-            sum_Ax = sum(A[i, j] * x[j] for j in range(n) if j != i)
-            x_new[i] = (b[i] - sum_Ax) / A[i, i]
-        if (np.linalg.norm(x - x_new) / np.linalg.norm(x)) < tol:
-            print("iteración", iteration)
-            return x_new
-        x = x_new
-    raise Exception("El método de Jacobi no convergió después de {} iteraciones".format(max_iter))
-    
 
-# Ejemplo de uso:
-if __name__ == "__main__":
-    x = 11
-    c = 1
-    # Definir la matriz de coeficientes A
-    A = np.array([[11-x, 5, 6],
-                 [-2, 4+c, 1],
-                 [-1, -1, 4]])
-
-    # Definir el vector de términos constantes b
-    b = np.array([15, 15, 20])
-
-    # Definir la aproximación inicial x0
-    x0 = np.array([1, 1, 1])
-
-    # Definir la tolerancia y el número máximo de iteraciones
-    tol = 0.1
-    max_iter = 1000
-
-    # Matiz T
-    T = jacobi_T_matrix(A)
-
-    # Radio espectral
-    print("Radio espectral: ", spectral_radius(T))
-
-    # Aplicar el método de Jacobi
-    solution = jacobi(A, b, x0, tol, max_iter)
-    print("Solución encontrada:")
-    print(solution)
+solve_jacobi(np.array([[5, 1, 0], [1, 5, 1], [0, 1, 5]]), np.array([6, 6, 6]), np.array([0, 0, 0]),1e-10,1000 )
