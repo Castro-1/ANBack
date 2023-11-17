cb98366f7c10b4a48a353e1fcdc28a7b90909907
Castro-1 <jecastrog@eafit.edu.co>
2023-11-17 17:53:01 -0500

    new deploy

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-import uvicorn
-from os import getenv
+from fastapi import FastAPI
+from routers import part1, part2, part3
+from fastapi.middleware.cors import CORSMiddleware
 
-if __name__ == "__main__":
-    port = int(getenv("PORT",8000))
-    uvicorn.run("api:app", host="0.0.0.0", port=port, reload=True)
+app = FastAPI()
+
+app.include_router(part1.router)
+app.include_router(part2.router)
+app.include_router(part3.router)
+
+origins = [
+    "http://127.0.0.1:5173/parte1/biseccion"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+async def root():
+    return "Hello, base path"
